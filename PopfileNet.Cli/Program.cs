4b995fda1c7228a6bf861dc3dc20d7f2fb2bd338
Blakharaz <1158859+blakharaz@users.noreply.github.com>
2026-02-22 12:15:52 +0100

    Use System.Commandline to encapsulate test functions in CLI project

--- PopfileNet.Cli/Program.cs
+++ PopfileNet.Cli/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.CommandLine;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -15,23 +14,47 @@ var configuration = configBuilder.Build();
 
 using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
 
-Console.WriteLine("Hello, World!");
-
 var settings = configuration.GetSection("ImapSettings").Get<ImapSettings>()
     ?? throw new InvalidOperationException("ImapSettings configuration not found");
 
-var imapService = new ImapClientService(Options.Create(settings), new Logger<ImapClientService>(factory));
-await imapService.TestConnectionAsync();
+// Create root command
+var rootCommand = new RootCommand("PopfileNet CLI - IMAP mail test utility");
 
-var folders = await imapService.GetAllPersonalFoldersAsync();
-if (folders.Count == 0)
+// Create limit option
+var limitOption = new Option<int>("--limit",
+    "-l", "Number of emails to fetch") { DefaultValueFactory = _ => 40 };
+
+// Create fetch-mails command
+var fetchMailsCommand = new Command("fetch-mails", "Fetch emails from the IMAP server");
+fetchMailsCommand.Options.Add(limitOption);
+
+fetchMailsCommand.SetAction(async (pr) =>
 {
-    Console.WriteLine("No folders found.");
-    return;
-}
+    var limit = pr.GetValue(limitOption);
+    
+    var imapService = new ImapClientService(Options.Create(settings), new Logger<ImapClientService>(factory));
+    await imapService.TestConnectionAsync();
 
-var folder = folders[Math.Min(folders.Count - 1, 10)];
-var mailIds = await imapService.FetchEmailIdsAsync(folder.FullName);
-var mails = await imapService.FetchEmailsAsync(mailIds.Take(40));
+    var folders = await imapService.GetAllPersonalFoldersAsync();
+    if (folders.Count == 0)
+    {
+        Console.WriteLine("No folders found.");
+        return 0;
+    }
 
-Console.WriteLine(mails.Count);
+    var folder = folders[Math.Min(folders.Count - 1, 10)];
+    var mailIds = await imapService.FetchEmailIdsAsync(folder.FullName);
+    var mails = await imapService.FetchEmailsAsync(mailIds.Take(limit).ToList());
+
+    Console.WriteLine($"Fetched {mails.Count} emails.");
+    return 0;
+});
+
+// Create test command
+var testCommand = new Command("test", "Test IMAP connection and operations");
+testCommand.Subcommands.Add(fetchMailsCommand);
+
+rootCommand.Subcommands.Add(testCommand);
+
+var parseResult = rootCommand.Parse(args);
+return await parseResult.InvokeAsync();
