b592f04faf620587eb63d86070b5d030a9f081d6
Blakharaz <1158859+blakharaz@users.noreply.github.com>
2026-04-18 21:23:39 +0200

    Enable dev mode by default

--- PopfileNet.AppHost/AppHost.cs
+++ PopfileNet.AppHost/AppHost.cs
@@ -4,9 +4,13 @@ var postgres = builder.AddPostgres("postgres")
     .WithDataVolume();
 
 var backend = builder.AddProject<Projects.PopfileNet_Backend>("popfilenet-backend")
-    .WithReference(postgres);
+    .WithReference(postgres)
+    .WithHttpEndpoint(port: 5000)
+    .WithEnvironment("DevMode__Enabled", "true");
 
 builder.AddProject<Projects.PopfileNet_Ui>("popfilenet-ui")
-    .WithReference(backend);
+    .WithReference(backend)
+    .WithHttpEndpoint(port: 5010)
+    .WithEnvironment("DevMode__Enabled", "true");
 
 builder.Build().Run();
